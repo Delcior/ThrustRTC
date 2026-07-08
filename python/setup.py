5976418e4b37888584e75a667c42d43f253234ad
Delcior <kacper.derlatka@outlook.com>
2026-07-09 01:11:01 +0200

    use cibuildwheel

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 from codecs import open
 import os
+import shutil
+import subprocess
 
 cmdclass = {}
 
@@ -19,12 +21,44 @@ def get_package_data():
 		return ['PyThrustRTC.dll']
 	elif os.name == "posix":
 		return ['libPyThrustRTC.so']
+	return []
+
+def build_native_library():
+	build_dir = os.path.join(here, '_cmake_build')
+	install_dir = os.path.join(build_dir, 'install')
+	config = os.environ.get('BUILD_TYPE', 'Release')
+	cmake_args = [
+		'cmake',
+		'-S', here,
+		'-B', build_dir,
+		'-DCMAKE_BUILD_TYPE=%s' % config,
+		'-DCMAKE_INSTALL_PREFIX=%s' % install_dir,
+	]
+	if os.name == 'nt' and 'CMAKE_GENERATOR' not in os.environ:
+		cmake_args.extend(['-G', 'NMake Makefiles'])
+	if os.name != 'nt':
+		cmake_args.append('-DCMAKE_CXX_FLAGS=-include cstdio')
+
+	subprocess.check_call(cmake_args)
+	subprocess.check_call(['cmake', '--build', build_dir, '--config', config])
+	subprocess.check_call(['cmake', '--install', build_dir, '--config', config])
+
+	for binary in get_package_data():
+		src = os.path.join(install_dir, 'test_python', 'ThrustRTC', binary)
+		dst = os.path.join(here, 'ThrustRTC', binary)
+		if not os.path.exists(src):
+			raise RuntimeError('Expected native library was not built: %s' % src)
+		shutil.copy2(src, dst)
 
 if bdist_wheel is not None:
 	class bdist_wheel_platform_tag(bdist_wheel):
 		def finalize_options(self):
 			bdist_wheel.finalize_options(self)
 			self.root_is_pure = False
+
+		def run(self):
+			build_native_library()
+			bdist_wheel.run(self)
 
 		def get_tag(self):
 			_, _, plat = bdist_wheel.get_tag(self)
